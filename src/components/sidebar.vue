f62f600efa1813acf96b8e7df987b6ecfafe003b
jovanni-dev <jovanni.millers123@outlook.com>
2023-02-13 05:23:24 -0500

    fix_sidebar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -19,13 +19,7 @@
                             </el-icon>
                             <span>{{ item.title }}</span>
                         </template>
-                        <template v-for="subItem in item.subs">
-                            <el-sub-menu
-                                v-if="subItem.subs"
-                                :index="subItem.index"
-                                :key="subItem.index"
-                                v-permiss="item.permiss"
-                            >
+
                                 <template #title>{{ subItem.title }}</template>
                                 <el-menu-item v-for="(threeItem, i) in subItem.subs" :key="i" :index="threeItem.index">
                                     {{ threeItem.title }}
